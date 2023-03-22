27edf4c33c66dbe6e953c372345e5cf4c78e136b
chenzeqi <vinroot@163.com>
2023-03-22 17:22:15 +0800

    feat: 更改demo

--- packages/theme/demo/.vitepress/config.ts
+++ packages/theme/demo/.vitepress/config.ts
@@ -5,17 +5,17 @@ import packageJSON from '../../package.json'
 const blogTheme = getThemeConfig({
     friend: [
         {
-            nickname: '粥里有勺糖',
-            des: '你的指尖用于改变世界的力量',
+            nickname: '99℃白开水',
+            des: '个人博客',
             avatar:
-                'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+                'http://cdn.chen-zeqi.cn/logo.jpg',
             url: 'https://sugarat.top'
         },
         {
-            nickname: 'Vitepress',
-            des: 'Vite & Vue Powered Static Site Generator',
+            nickname: '@99_water/module',
+            des: 'a vue module',
             avatar:
-                'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTI2NzY1Ng==674995267656',
+                'http://cdn.chen-zeqi.cn/logo.jpg',
             url: 'https://vitepress.vuejs.org/'
         }
     ],
@@ -71,67 +71,22 @@ const extraHead: any =
 
 export default defineConfig({
     lang: 'zh-cmn-Hans',
-    title: '@sugarat/theme',
-    description: '粥里有勺糖的博客主题，基于 vitepress 实现',
+    title: '@99_water/theme',
     head: [...extraHead],
     vite: {
         server: {
             host: '0.0.0.0'
-        },
-        resolve: {
-            alias: {
-                '@sugarat/theme': path.join(__dirname, '../../src/index.ts')
-            }
         }
     },
     themeConfig: {
         ...blogTheme,
-        footer: {
-            message: `Power By <a target="_blank" href="https://theme.sugarat.top/"> @sugarat/theme@${packageJSON.version} </a>`,
-            copyright: 'MIT Licensed | Copyright © 粥里有勺糖'
-        },
-        nav: [
-            {
-                text: `v${packageJSON.version}`,
-                link: '/changelog'
-            },
-            {
-                text: '线上作品',
-                items: [
-                    {
-                        text: '轻取(文件收集)',
-                        link: 'https://ep2.sugarat.top'
-                    },
-                    {
-                        text: '个人图床',
-                        link: 'https://imgbed.sugarat.top'
-                    },
-                    {
-                        text: '考勤小程序',
-                        link: 'https://hdkq.sugarat.top/'
-                    },
-                    {
-                        text: '时光恋人',
-                        link: 'https://lover.sugarat.top'
-                    },
-                    {
-                        text: '在线简历生成',
-                        link: 'https://resume.sugarat.top/'
-                    }
-                ]
-            }
-        ],
+        nav: [],
         socialLinks: [
             {
                 icon: 'github',
                 link: 'https://github.com/ATQQ/sugar-blog/tree/master/packages/theme'
             }
         ],
-        editLink: {
-            pattern:
-                'https://github.com/ATQQ/sugar-blog/tree/master/packages/theme/demo/:path',
-            text: '去 GitHub 上编辑内容'
-        },
         lastUpdatedText: '上次更新于'
     }
 })
--- packages/theme/demo/index.md
+++ packages/theme/demo/index.md
@@ -1,9 +1,8 @@
 ---
 layout: home
 # 首页部分元素定制
-name: "99℃白开水"
 blog:
- name: '@sugarat/theme'
+ name: '@99_water/theme'
  motto: 简约风的 Vitepress 博客主题
  inspiring: 基于 Vitepress 定制的主题🎨
 ---
